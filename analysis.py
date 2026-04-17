8295cd46209d96c210747753a99ae95286f561d8
jlisete10-ship-it <jlisete10@gmail.com>
2026-04-17 22:16:21 +0100

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,8 @@
+customer,product,value,quantity,total_spent
+Ana,Keyboard,60,1.0,60.0
+Rui,Monitor,200,2.0,400.0
+unknown,Mouse,0,3.0,0.0
+Marta,unknown,150,1.0,150.0
+Rui,Keyboard,90,2.0,180.0
+Ana,Monitor,0,1.0,0.0
+unknown,Chair,120,1.0,120.0
